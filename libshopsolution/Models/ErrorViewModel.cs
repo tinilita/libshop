0d243b2294d81258a604d6d1374d4ebe7bfb5ac4
Kristina Kapustina <kristinakapk@gmail.com>
2021-11-25 14:32:35 +0300

    First commot(1)

---
+++ libshopsolution/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace libshopsolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
